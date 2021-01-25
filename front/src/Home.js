e75d2aabc639823af6fd87555cb601a041c619df
geneaky <kaktus418@gmail.com>
2021-01-25 23:47:32 +0900

    'marker'

--- front/src/Home.js
+++ front/src/Home.js
@@ -1,22 +1,29 @@
 /*global kakao*/
-import React,{useEffect,useState} from 'react';
+import React,{useEffect} from 'react';
 import './Home.css';
 import WholeCourse from './WholeCourse'
 import CourseMenu from './CourseMenu'
 
 const Home = () => {
-  useEffect(async()=>{
-      await kakao.maps.load(()=>{
-        let container = document.getElementById('map');
-        let options = { 
-	            center: new kakao.maps.LatLng(37.564213, 127.001698), 
-	            level: 5 
-            };
-        const map = new kakao.maps.Map(container, options);
-      })
-  },[]);
+  useEffect(()=>{mapScript()},[]);
 
-  sdfsf
+  const mapScript = () => {
+
+      const container = document.getElementById('map');
+      const options = { 
+            center: new kakao.maps.LatLng(37.564213, 127.001698), 
+            level: 5 
+          };
+      const map = new kakao.maps.Map(container, options);
+
+      kakao.maps.event.addListener(map,'click',function(mouseEvent){
+        let marker = new kakao.maps.Marker({
+          map:map,
+          position: new kakao.maps.LatLng(mouseEvent.latLng.Ma,mouseEvent.latLng.La)
+        })
+        marker.setMap(map)
+      })
+  }
 
   return (
     <div className="Home">
