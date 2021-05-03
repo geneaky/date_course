6044677b41e67b99f511993266b5ce0538061496
geneaky <kaktus418@gmail.com>
2021-05-03 20:51:10 +0900

    fine tune

--- back/src/main/java/me/toy/server/security/jwt/JwtTokenProvider.java
+++ back/src/main/java/me/toy/server/security/jwt/JwtTokenProvider.java
@@ -3,40 +3,38 @@ package me.toy.server.security.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import me.toy.server.config.AppProperties;
 import me.toy.server.security.oauth2.user.UserPrincipal;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.Date;
 
 @Service
+@RequiredArgsConstructor
 public class JwtTokenProvider {
-    public static final String tokenSecret = "geneaky";
-    public static final String tokenExpirationMsec = "86400000";
+
+    private final AppProperties appProperties;
 
     public String create(Authentication authentication){
         UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
 
-        LocalDateTime localDateTime = LocalDateTime.now();
-        int sec = Integer.parseInt(tokenExpirationMsec)/1000;
-        localDateTime = localDateTime.plusSeconds(sec);
-        ZoneId defaultZoneId = ZoneId.systemDefault();
-        Date expireDate = Date.from(localDateTime.atZone(defaultZoneId).toInstant());
+        Date now = new Date();
+        Date expireDate = new Date(now.getTime() + appProperties.getAuth().getTokenExpirationMsec());
 
         String token = Jwts.builder()
                 .setSubject(Long.toString(principal.getId()))
                 .setIssuedAt(new Date())
                 .setExpiration(expireDate)
-                .signWith(SignatureAlgorithm.HS256,tokenSecret).compact();
+                .signWith(SignatureAlgorithm.HS256,appProperties.getAuth().getTokenSecret()).compact();
 
         return token;
     }
 
     public Claims getClaims(String token){
         Claims claims = Jwts.parser()
-                .setSigningKey(tokenSecret)
+                .setSigningKey(appProperties.getAuth().getTokenSecret())
                 .parseClaimsJws(token)
                 .getBody();
 
@@ -44,17 +42,13 @@ public class JwtTokenProvider {
     }
 
     public long getUserIdFromToken(String token){
-        Claims claims = Jwts.parser()
-                .setSigningKey(tokenSecret)
-                .parseClaimsJws(token)
-                .getBody();
-
+        Claims claims = getClaims(token);
         return Long.parseLong(claims.getSubject());
     }
 
     public boolean validateToken(String authToken){
         try{
-            Jwts.parser().setSigningKey(tokenSecret).parseClaimsJws(authToken);
+            Jwts.parser().setSigningKey(appProperties.getAuth().getTokenSecret()).parseClaimsJws(authToken);
             return true;
         }catch(Exception e){
             e.printStackTrace();
