ab1f179cb70aea56572d653dc54b2703e60d8f7f
geneaky <kaktus418@gmail.com>
2021-05-27 18:50:41 +0900

    have to regist datecoures with user id

--- back/src/main/java/me/toy/server/controller/DateCourseController.java
+++ back/src/main/java/me/toy/server/controller/DateCourseController.java
@@ -6,18 +6,16 @@ import me.toy.server.entity.DateCourse;
 import me.toy.server.entity.Location;
 import me.toy.server.repository.DateCourseRepository;
 import me.toy.server.repository.LocationRepository;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
+import java.beans.PropertyEditorSupport;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -37,10 +35,11 @@ public class DateCourseController {
         Path directory = Paths.get("src/main/resources/imageUpload/").toAbsolutePath().normalize();
 
         for(RegistDateCourseRequestDto requestDto: requestDtoList.getLocationList()){
-            if(requestDto.getFile().isEmpty()&&requestDto.getFile()==null){
-//                Location location = new Location(requestDto,"");
-                Location location = new Location(requestDto.getPlaceName(),requestDto.getText(),"",requestDto.getPosX(),requestDto.getPosY());
+            if(requestDto.getFile()==null){
+                System.out.println("파일이 없어요");
+                Location location = new Location(requestDto,"");
                 location.setDateCourse(dateCourse);
+                locationRepository.save(location);
                 continue;
             }
             String fileSaveName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS"));
--- back/src/main/java/me/toy/server/dto/RegistDateCourseRequestDto.java
+++ back/src/main/java/me/toy/server/dto/RegistDateCourseRequestDto.java
@@ -6,14 +6,11 @@ import lombok.Setter;
 import org.springframework.lang.Nullable;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Optional;
-
 @Getter
 @Setter
 @NoArgsConstructor
 public class RegistDateCourseRequestDto {
 
-    @Nullable
     private MultipartFile file;
     private String placeName;
     private float posX;
